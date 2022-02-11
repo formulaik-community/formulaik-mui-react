3d0e6ef9937e6369db5079d41a90a755d5bfcabf
Aboubacar Doucoure <yelounak@gmail.com>
2022-02-11 16:21:50 +0100

    Changed textAreaAutoResize

--- src/textArea/index.js
+++ src/textArea/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import TextareaAutosize from 'react-textarea-autosize'
+import TextareaAutosize from '@mui/material/TextareaAutosize'
 
 export default (props) => {
-    const { customOnValueChanged, field, errors, item: { subType, id } } = props
-
+    const { customOnValueChanged, field, errors, item: { props: itemProps = {}, id } } = props
+    const { maxRows = 1000, minRows = 3, placeholder } = itemProps
     return <TextareaAutosize
-        {...field}
+        aria-label="minimum height"
+        minRows={maxRows}
+        minRows={minRows}
         onChange={({ target: { value } }) => customOnValueChanged(value)}
-        className={`textarea h-24 textarea-bordered text-base ${errors[id] ? 'bg-red-100 border-red-600' : 'border-warmGray-400'}`}
+        placeholder={placeholder}
+        {...field}
+        className={`textarea h-64 textarea-bordered text-base ${errors[id] ? 'bg-red-100 border-red-600' : 'border-warmGray-400'}`}
     />
 }
